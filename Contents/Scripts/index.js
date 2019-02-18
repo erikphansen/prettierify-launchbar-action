1386d976e9484ad34556f57802b4e35c364f3fc9
Erik P. Hansen <eph@erikphansen.com>
2019-02-18 08:02:55 -0800

    documentation

--- Contents/Scripts/index.js
+++ Contents/Scripts/index.js
@@ -3,26 +3,36 @@ import jsParsers from 'prettier/parser-babylon';
 import cssParsers from 'prettier/parser-postcss';
 import htmlParsers from 'prettier/parser-html';
 
-// Language name, parser to use, icon prefix
+// Language name, parser to use, icon file name. Stored as tuples instead of
+// objects for simplicity's sake.
 const languageTypes = [
-  ['JSON', 'json', 'json'],
-  ['JavaScript', 'babel', 'js'],
-  ['CSS', 'css', 'css'],
-  ['Sass', 'scss', 'scss'],
-  ['HTML', 'html', 'html'],
+  ['JSON', 'json', 'json-icon-Template'],
+  ['JavaScript', 'babel', 'js-icon-Template'],
+  ['CSS', 'css', 'css-icon-Template'],
+  ['Sass', 'scss', 'scss-icon-Template'],
+  ['HTML', 'html', 'html-icon-Template'],
 ];
 
-// This is the function that does all the work
-// The settings object has the input to format and the parser (language type) to
-// format the input as
-export function format(settings) {
+/**
+ * The function that handles the formatting. Because it's called via a LaunchBar
+ * item, it can only accept a single argument. The arg will contain the text to
+ * format as well as the language we want to format the text as.
+ *
+ * @export
+ * @param {Object} options - formatting options
+ * @param {string} options.input - the string to format
+ * @param {string} options.parser - the parser for Prettier to use
+ * @returns - either returns the formatted text or pastes the formatted text and
+ * exits
+ */
+export function format(options) {
   let prettierrc = {};
   try {
     prettierrc = File.readJSON('~/.prettierrc');
   } catch (error) {}
-  const formatted = prettier.format(settings.input, {
+  const formatted = prettier.format(options.input, {
     ...prettierrc,
-    parser: settings.parser,
+    parser: options.parser,
     plugins: [jsParsers, cssParsers, htmlParsers],
   });
   // If SHIFT is held down when running the action, paste the formatted code
@@ -34,15 +44,30 @@ export function format(settings) {
   }
 }
 
+/**
+ * Returns an array of LaunchBar items representing the list of languages the
+ * user can format their snippet as.
+ *
+ * @param {string} input - The text to format
+ * @returns {Object[]} - An array of objects for LaunchBar to display to the
+ * user
+ */
 function promptForLanguageType(input) {
   return languageTypes.map(([title, parser, icon]) => ({
     title,
     action: 'format',
     actionArgument: { input, parser },
-    icon: `${icon}-icon-Template`,
+    icon,
   }));
 }
 
+/**
+ * The default LaunchBar action
+ *
+ * @export
+ * @param {string} argument - The text to format
+ * @returns - A list of languages the text can be formatted as
+ */
 export function run(argument) {
   if (argument == undefined) {
     // Inform the user that there was no argument
